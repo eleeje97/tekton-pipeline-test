af1a3ffd0b6cf336fc10ea39576983086edd9f53
danalee <eleeje97@gmail.com>
2023-05-15 10:38:36 +0900

    init commit

---
+++ tekton-pipeline-test-application/src/test/java/com/example/tektonpipelinetestapplication/TektonPipelineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tektonpipelinetestapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TektonPipelineTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
